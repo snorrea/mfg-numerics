Gradient needs fixing
Found a way to make the numpy.gradient function work. It does however
make the solution look worse than it did before. Look at the last
definition of the gradient to see what went wrong or somthing I don't
fukcing care anymore.

semilagrange.py
@@ -11,7 +11,7 @@ from matplotlib import cm
 
 #INPUTS
 dx = 1/10 #these taken from Gueant's paper
-dt = 1/50
+dt = 1/10
 xmin = 0
 xmax = 1
 T = 1
@@ -90,24 +90,20 @@ for n in range (0,Niter):
 		v_tmp = np.copy(v[((k+1)*I):((k+1)*I+I)])
 		for i in range (0,I):
 			#print i,k
-			tmp = minimize(tau,0,args=(i,k,v_tmp,x))
+			tmp = minimize(tau,0,args=(i,k,v_tmp,x)) #this dominates!
 			v[index(i,k)] = dt*F(x[i],m_old) + tmp.fun
 	print "Spent time", time.time()-temptime
 	##compute the control based on the convoluted potential v
 	#convolute the potential in space
 		#let's skip that for now
-
 	
-	print "Computing iteration", n, "of gradient of v..."
-	#find the gradient in space, which happens to be the fucking control/state/whatever
-	#do it manually because someone is a fucking moron
-	temptime = time.time()
-	for k in range (0,K-1): #gradient in last timestep and last spacestep is fucked
+	
+	#for k in range (0,K-1): #gradient in last timestep and last spacestep is fucked
 		#print I*k+I
 		#v_grad[(I*k):(I*k+I)] = np.gradient(v[(I+k):(I*k+I)],dx)
-		v_grad[(I*k)] = -(v[I*k]-v[I*k+1])/dx #backward difference
-		v_grad[(I*k)+I+1] = -(v[I*k+I]-v[I*k+I+1])/(dx)
-		v_grad[(I*k+1):(I*k+I)] = -(v[(I*k):(I*k+I-1)]-v[(I*k+2):(I*k+I+1)])/(2*dx)
+	#	v_grad[(I*k)] = -(v[I*k]-v[I*k+1])/dx #backward difference
+	#	v_grad[(I*k)+I+1] = (v[I*k+I]-v[I*k+I+1])/(dx)
+	#	v_grad[(I*k+1):(I*k+I)] = -(v[(I*k):(I*k+I-1)]-v[(I*k+2):(I*k+I+1)])/(2*dx)
 	print "Spent time", time.time()-temptime
 	#print v_grad
 
@@ -115,16 +111,16 @@ for n in range (0,Niter):
 	#this is where it fucks up
 	#set initial conditions again I guess?
 #	m[0:I] = np.copy(m0)
-
+	print m.shape
 	print "Computing iteration", n, "of m..."
 	temptime = time.time()
 	for k in range(0,K-1):
+		v_grad = np.gradient(v[(I*k):(I*k+I)])
+		print v_grad
 		for i in range (0,I):
 			m[index(i,k+1)] = 0
-			#if m[index(i,k)] > 2: #stuff will fuck up here if the gradient is weird
-			#	print i,k,m[index(i,k)]
 			for j in range (0,I):
-				m[index(i,k+1)] += beta(x[j]-dt*v_grad[index(j,k)],i,x)*m[index(j,k)]
+				m[index(i,k+1)] += beta(x[j]-dt*v_grad[j],i,x)*m[index(j,k)]
 	print "Spent time", time.time()-temptime
 	#compute norms of stuff
 	mchange = m-m_old
@@ -152,10 +148,10 @@ for i in range (0,I):
 	for k in range (0,K):
 		msoln[i,k] = m[index(i,k)]
 		vsoln[i,k] = v[index(i,k)]
-		gradsoln[i,k] = v_grad[index(i,k)]
+#		gradsoln[i,k] = v_grad[index(i,k)]
 msoln = np.transpose(msoln)
 vsoln = np.transpose(vsoln)
-gradsoln = np.transpose(gradsoln)
+#gradsoln = np.transpose(gradsoln)
 #cut the change vectors with kMax
 ml1 = ml1[:kMax]
 ml2 = ml2[:kMax]
@@ -209,13 +205,13 @@ ax4.set_xlabel('Iteration number')
 ax4.set_ylabel('Log10 of change')
 fig4.suptitle('Convergence of v(x,t)', fontsize=14)
 #plot gradient
-fig5 = plt.figure(5)
-ax5 = fig5.add_subplot(111, projection='3d')
-ax5.plot_surface(Xplot,Tplot,gradsoln,rstride=5,cstride=5,cmap=cm.coolwarm,linewidth=0, antialiased=False)
-ax5.set_xlabel('x')
-ax5.set_ylabel('t')
-ax5.set_zlabel('grad v(x,t)')
-fig5.suptitle('Solution of gradient of v(x,t)', fontsize=14)
+#fig5 = plt.figure(5)
+#ax5 = fig5.add_subplot(111, projection='3d')
+#ax5.plot_surface(Xplot,Tplot,gradsoln,rstride=5,cstride=5,cmap=cm.coolwarm,linewidth=0, antialiased=False)
+#ax5.set_xlabel('x')
+#ax5.set_ylabel('t')
+#ax5.set_zlabel('grad v(x,t)')
+#fig5.suptitle('Solution of gradient of v(x,t)', fontsize=14)
 plt.show()
 
 