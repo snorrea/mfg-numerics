f32c61897b5a254f9076b0e62163b608bac45918
snorrea <towerofangels@gmail.com>
2015-02-25 18:17:04 +0100

    Before the minus flip in the HJB

--- matrix_gen.py
+++ matrix_gen.py
@@ -131,7 +131,7 @@ def u_matrix_explicit(f1_array,f2_array,D11,D22,D12,I,J,dx,dt):
 	return lamb*sparse.csr_matrix(output)
 
 #####################
-def u_matrix_implicit(f1_array,f2_array,D11,D22,D12,I,J,dx,dt): #THIS JUST WORKS
+def m_matrix_implicit(f1_array,f2_array,D11,D22,D12,I,J,dx,dt): #THIS JUST WORKS
 	output = np.zeros((I*J,I*J))
 	lamb = dt/(dx**2)
 	xbound1 = range(0,I)
@@ -145,57 +145,72 @@ def u_matrix_implicit(f1_array,f2_array,D11,D22,D12,I,J,dx,dt): #THIS JUST WORKS
 	f1_array = np.ravel(f1_array)
 	f2_array = np.ravel(f2_array)
 	for i in range(0,I*J):
-#		d12 = D12[i]
-#		d11 = D11[i]
-#		d22 = D22[i]
-#		f1 = f1_array[i]
-#		f2 = f2_array[i]
-		output[i,i] += 1/lamb + dx*(max(0.5*(f1_array[i]+f1_array[i+1]),0)+max(0.5*(f2_array[i]+f2_array[i+1]),0)-min(0.5*(f1_array[i]+f1_array[i-1]),0)-min(0.5*(f2_array[i]+f2_array[i-1]),0)) #term i)
-		output[i,i] += -max(0,D11[i+1]-D11[i]+0.25*(D12[i+1+I]+D12[i+I]-D12[i-I]-D12[i+1-I])) - max(0,D22[i+I]-D22[i]+0.25*(D12[i+1+I]+D12[i+1]-D12[i-1]-D12[i-1+I])) #term ii) part 1
-		output[i,i] += min(0,D11[i]-D11[i-1]+0.25*(D12[i+I]+D12[i-1+I]-D12[i-I]-D12[i-1-I])) + min(0,D22[i]-D22[i-I]+0.25*(D12[i+1]+D12[i+1-I]-D12[i-1]-D12[i-1-I])) #term ii) part 2
-		output[i,i] += 0.5*(D11[i+1]+D22[i+I]-D11[i-1]-D22[i-I]) #term iii)
-		#avoid segfaults
-		if not ismember_sorted(i,xbound1): #allows (i,j-1)
+		output[i,i] += 1/lamb
+		if not ismember_sorted(i,xbound1) and not ismember_sorted(i,xbound2) and not ismember_sorted(i,ybound1) and not ismember_sorted(i,ybound2):
+			#center terms
+			output[i,i] += dx*(max(0.5*(f1_array[i]+f1_array[i+1]),0)+max(0.5*(f2_array[i]+f2_array[i+1]),0)-min(0.5*(f1_array[i]+f1_array[i-1]),0)-min(0.5*(f2_array[i]+f2_array[i-1]),0)) #term i)
+			output[i,i] += -max(0,D11[i+1]-D11[i]+0.25*(D12[i+1+I]+D12[i+I]-D12[i-I]-D12[i+1-I])) - max(0,D22[i+I]-D22[i]+0.25*(D12[i+1+I]+D12[i+1]-D12[i-1]-D12[i-1+I])) #term ii) part 1
+			output[i,i] += min(0,D11[i]-D11[i-1]+0.25*(D12[i+I]+D12[i-1+I]-D12[i-I]-D12[i-1-I])) + min(0,D22[i]-D22[i-I]+0.25*(D12[i+1]+D12[i+1-I]-D12[i-1]-D12[i-1-I])) #term ii) part 2
+			output[i,i] += 0.5*(D11[i+1]+D22[i+I]-D11[i-1]-D22[i-I]) #term iii)
+			#other terms
 			output[i,i-I] += max(0.5*(f2_array[i-I]+f2_array[i]),0)/h #term i)
 			output[i,i-I] += max(0,D22[i]-D22[i-I]+0.25*(D12[i+1]+D12[i+1-I]-D12[i-1]-D12[i-1-I])) #term ii), conflict
 			output[i,i-I] += -(-0.125*(D12[i+1]+D12[i]) + 0.5*(D22[i]+D22[i-I]) + 0.125*(D12[i]+D12[i-1]) )) #term iii), conflict
-		if not ismember_sorted(i,xbound2): #allows (i,j+1)
 			output[i,i+I] += min(0.5*(f2_array[i+I]+f2_array[i]),0)/h #term i)
 			output[i,i+I] += -min(0,D22[i+I]-D22[i]+0.25*(D12[i+1+I]+D12[i+1]-D12[i-1]-D12[i-1+I])) #term ii), conflict
 			output[i,i+I] += -(0.125*(D12[i+1]+D12[i])+0.5*(D22[i]+D22[i+I]) - 0.125*(D12[i]+D12[i-1]) ) #term iii), conflict
-		if not ismember_sorted(i,ybound1): #allows (i-1,j) 
 			output[i,i-1] += max(0.5*(f1_array[i-1]+f1_array[i]),0)/h #term i)
 			output[i,i-1] += max(0,D11[i]-D11[i-1]+0.25*(D12[i+I]+D12[i-1+I]-D12[i-I]-D12[i-1-I])) #term ii), conflict
 			output[i,i-1] += -(-0.125*(D12[i]+D12[i+I])+0.5*(D11[i]+D11[i-1])+0.125*(D12[i]+D12[i-I])) #term iii), conflict
-		if not ismember_sorted(i,ybound2): #allows (i+1,j)
 			output[i,i+1] += min(0.5*(f1_array[i+1]+f1_array[i]),0)/h #term i)
 			output[i,i+1] += -min(0,D11[i+1]-D11[i]+0.25*(D12[i+1+I]+D12[i+I]-D12[i-I]-D12[i+1-I])) #term ii), conflict
 			output[i,i+1] += -(0.5*(D11[i]+D11[i+1]) + 0.125*(D12[i]+D12[i+I]) - 0.125*(D12[i]+D12[i-I])) #term iii), conflict
-		if not ismember_sorted(i,xbound1) and not ismember_sorted(i,ybound1): #allows (i-1,j-1), no conflicts
-			output[i,i-1-I] += -0.125*(D12[i]+D12[i-1]+D12[i]+D12[i-I])
-		if not ismember_sorted(i,xbound2) and not ismember_sorted(i,ybound2): #allows (i+1,j+1), no conflicts
-			output[i,i+1+I] += -0.125*(D12[i]+D12[i+1]+D12[i]+D12[i+I])
-		if not ismember_sorted(i,xbound1) and not ismember_sorted(i,ybound2): #allows (i+1,j-1), no conflicts
-			output[i,i+1-I] += 0.125*(D12[i]+D12[i+1]+D12[i]+D12[i-I])
-		if not ismember_sorted(i,ybound1) and not ismember_sorted(i,xbound2): #allows (i-1,j+1), no conflicts
-			output[i,i-1+I] += 0.125*(D12[i]+D12[i-1]+D12[i]+D12[i+I])
-		#then add boundary conditions; this is mostly guesswork
-		if ismember_sorted(i,xbound1): #allows (i,j-1)
-			output[i,i+I] += 0
-		if ismember_sorted(i,xbound2): #allows (i,j+1)
-			output[i,i-I] += 0
-		if ismember_sorted(i,ybound1): #allows (i-1,j) 
-			output[i,i+1] += 0
-		if ismember_sorted(i,ybound2): #allows (i+1,j)
+		elif ismember_sorted(i,xbound1) and ismember_sorted(i,ybound1): #SOUTH-WEST
+			output[i,i] += 0
+			output[i,i] += 0
+			output[i,i] += 0.5*(-(D11[i+1]-D11[i]) -0.25*(D11[i]+D11[i+1]+D11[i-I]+D11[i+1-I]) + 0.5*(D12[i]+D12[i+1]+D12[i-I]+D12[i+1-I]) -(D12[i]+D12[i+1]) - (D12[i]+D12[i-I]) + (D22[i]+D22[i-I]) + 0.5*(D22[i]+D22[i+1]+D22[i-I]+D22[i+1-I]) ) #term iii)
+			output[i,i+1] += 0.5 #term i)
+			output[i,i+1] += 0.5 #term ii)
+			output[i,i+1] += 0.5 #term iii)
+			output[i,i+I] += 0.5
+			output[i,i+I] += 0.5
+			output[i,i+I] += 0.5
+			output[i,i+1+I] += 0.5
+			output[i,i+1+I] += 0.5
+			output[i,i+1+I] += 0.5
+		elif ismember_sorted(i,xbound2) and ismember_sorted(i,ybound2): #NORTH-EAST
+			output[i,i] += 0
 			output[i,i-1] += 0
-		if ismember_sorted(i,xbound1) and ismember_sorted(i,ybound1): #allows (i-1,j-1)
-			output[i,i+1+I] += 0
-		if ismember_sorted(i,xbound2) and ismember_sorted(i,ybound2): #allows (i+1,j+1)
+			output[i,i-I] += 0
 			output[i,i-1-I] += 0
-		if ismember_sorted(i,xbound1) and ismember_sorted(i,ybound2): #allows (i+1,j-1)
+		elif ismember_sorted(i,xbound1) and ismember_sorted(i,ybound2): #SOUTH-EAST
+			output[i,i] += 0
+			output[i,i-1] += 0
+			output[i,i+I] += 0
 			output[i,i-1+I] += 0
-		if ismember_sorted(i,ybound1) and ismember_sorted(i,xbound2): #allows (i-1,j+1)
+		elif ismember_sorted(i,ybound1) and ismember_sorted(i,xbound2): #NORTH-WEST
+			output[i,i] += 0
+			output[i,i+1] += 0
+			output[i,i-I] += 0
 			output[i,i+1-I] += 0
+		elif ismember_sorted(i,xbound1): #SOUTH
+			output[i,i] += 0
+			output[i,i+1] += 0
+			output[i,i+I] += 0
+			output[i,i+1+I] += 0
+		elif ismember_sorted(i,xbound2): #NORTH
+			output[i,i] += 0
+			output[i,i+1] += 0
+			output[i,i+I] += 0
+			output[i,i+1+I] += 0
+		elif ismember_sorted(i,ybound1): #WEST
+			output[i,i] += 0
+			output[i,i+1] += 0
+			output[i,i+I] += 0
+			output[i,i+1+I] += 0
+		elif ismember_sorted(i,ybound2): #SOUTH
+			output[i,i-1] += 0
+
 	return lamb*sparse.csr_matrix(output)
 
 
